8edf1d080540bcb60c15885abc5272cb4e4a0875
Cresence <devharris93@gmail.com>
2023-08-03 20:28:24 -0400

    Restructured Description to better fit instructions.

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,24 +1,39 @@
 import styles from "./Description.module.css";
 import { Component } from "react";
 
-class RecipeAuthor extends Component {
-    
+const RecipeAuthor = () => {
+    let authorLink, authorPhoto, authorName;
+
     // Data for later:
     // authorLink = "https://en.wikipedia.org/wiki/Chef_Boyardee";
     // authorPhoto = "https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Chefboyardeepic.jpg/220px-Chefboyardeepic.jpg";
     // authorName = "Ettore Boiardi"
+
+    return (
+        <div className = {styles.recipeAuthorBlock}>
+           <img src={authorPhoto} alt = "Reasonable alt text" className={styles.imageUpdates} />
+           <div>
+              <h3>{authorName}</h3>
+              <a href={authorLink}>Blog name</a> 
+           </div>
+        </div>
+     );
+}
+
+class RecipeDescription extends Component {
     render() {
-        let authorLink, authorPhoto, authorName;
         return (
-            <div className = {styles.recipeAuthorBlock}>
-               <img src={authorPhoto} alt = "Reasonable alt text" className={styles.imageUpdates} />
-               <div>
-                  <h3>{authorName}</h3>
-                  <a href={authorLink}>Blog name</a> 
-               </div>
+            <div> 
+            <div>
+               <h1>Recipe Title</h1>
+               <p>Short recipe description</p>
             </div>
-         );
+            <RecipeAuthor />
+         </div>
+        )
     }
 }
 
-export default RecipeAuthor;
+
+
+export default RecipeDescription;
