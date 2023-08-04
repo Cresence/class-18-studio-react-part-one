79ed1f0f3c810f0713abf24e079ffcb632da8f46
Cresence <devharris93@gmail.com>
2023-08-03 20:39:02 -0400

    Created Recipe Photo component and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import RecipeDescription from './components/Description';
 import RecipeIngredients from './components/Ingredients';
+import RecipePhoto from './components/Photos';
 
 
 function App() {
   return (
     <div className="App">
+      <RecipePhoto />
       <div>
         <RecipeDescription />
         <RecipeIngredients />
---
+++ src/components/Photos.js
@@ -0,0 +1,7 @@
+import styles from "./Photos.module.css";
+
+export default function RecipePhoto() {
+    return(
+        <img src="valid URL to recipe photo" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
